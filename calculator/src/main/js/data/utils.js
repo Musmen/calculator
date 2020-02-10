bccc416b3ae06a8487da5f054dd938a430c86b71
Igor Musmen <musmen@tut.by>
2020-02-11 00:18:32 +0300

    fix: implement correct monthly payment calculations

--- calculator/src/main/js/data/utils.js
+++ calculator/src/main/js/data/utils.js
@@ -2,12 +2,28 @@ function getTaxes(postCode) {
   return postCode.toString().split('').map((item) => `${item * 11} `);
 }
 
+function calculateCreditValue(creditScore) {
+  let creditScoreValue;
+  if (creditScore >= 750) {
+    creditScoreValue = 0.95;
+  } else if (creditScore >= 700) {
+    creditScoreValue = 1;
+  } else if (creditScore >= 640) {
+    creditScoreValue = 1.05;
+  } else {
+    creditScoreValue = 1.2;
+  }
+  return creditScoreValue;
+}
+
 function calculateLoanMonthlyPayment(msrp, tradeIn, downPayment, loanTerm, creditScore, apr) {
-  return ((msrp - tradeIn - downPayment) * apr * creditScore) / loanTerm;
+  const creditScoreValue = calculateCreditValue(creditScore);
+  return ((msrp - tradeIn - downPayment) * (apr / 100) * creditScoreValue) / loanTerm;
 }
 
 function calculateLeaseMonthlyPayment(msrp, tradeIn, downPayment, leaseTerm, creditScore, mileage) {
-  return ((msrp - tradeIn - downPayment) * mileage * creditScore) / (10000 * leaseTerm);
+  const creditScoreValue = calculateCreditValue(creditScore);
+  return ((msrp - tradeIn - downPayment) * mileage * creditScoreValue) / (10000 * leaseTerm);
 }
 
 export { getTaxes, calculateLoanMonthlyPayment, calculateLeaseMonthlyPayment };
